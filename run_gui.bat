d397d5891acb980dc2a02bc4b22c045afc786c53
doktorravlik-svg <doktorravlik@gmail.com>
2026-04-15 02:37:24 +0300

    Add files via upload

--- run_gui.bat
+++ run_gui.bat
@@ -2,7 +2,7 @@
 chcp 65001 >nul
 echo Запуск RimWorld Translator Grabber GUI...
 cd /d "%~dp0"
-F:\CTk_Project_2026\Python314\python.exe run_gui.py
+python.exe run_gui.py
 if errorlevel 1 (
     echo.
     echo ОШИБКА! Нажмите любую клавишу...
